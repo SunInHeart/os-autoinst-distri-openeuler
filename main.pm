b0882d97a62158f297a59ffb94a3bf6f476a88ac
SunInHeart <2192455541@qq.com>
2024-11-29 22:17:31 +0800

    add mugen test

--- main.pm
+++ main.pm
@@ -6,11 +6,12 @@ use testapi;
 use autotest;
 
 # For xfce test
-# autotest::loadtest 'tests/xfce/boot.pm';
-# autotest::loadtest 'tests/xfce/terminal.pm';
-# autotest::loadtest 'tests/xfce/neofetch.pm';
+autotest::loadtest 'tests/xfce/boot.pm';
+autotest::loadtest 'tests/xfce/terminal.pm';
+autotest::loadtest 'tests/xfce/neofetch.pm';
+autotest::loadtest 'tests/xfce/mugen.pm';
 
 # For base test
-autotest::loadtest 'tests/base/boot.pm';
+# autotest::loadtest 'tests/base/boot.pm';
 
 1;
